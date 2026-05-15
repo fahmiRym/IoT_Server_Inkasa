199f2fedd24a1ccb6aba7c77d958d9b5cd750b43
fahmiRym <rf.ilmi@unim.ac.id>
2026-05-15 15:24:41 +0700

    Fix insecure form submission and force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (str_contains(request()->getHost(), 'inkalum.com')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
